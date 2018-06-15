fa4739bdbd473ec00be4a16c83dd924669960963
stc <stc@binaura.net>
2018-06-15 12:05:13 +0200

    updated pose scene

--- 010_pose-scene/sketch.js
+++ 010_pose-scene/sketch.js
@@ -1,6 +1,7 @@
 /*
 
 A scene where multiple participants are involved in the interaction
+todo: add multiple head positions
 
 */
 
@@ -11,6 +12,23 @@ let poseNet;
 let poses = [];
 let skeletons = [];
 
+// physics for playful interaction
+let VerletPhysics2D = toxi.physics2d.VerletPhysics2D,
+    VerletParticle2D = toxi.physics2d.VerletParticle2D,
+    AttractionBehavior = toxi.physics2d.behaviors.AttractionBehavior,
+    GravityBehavior = toxi.physics2d.behaviors.GravityBehavior,
+    Vec2D = toxi.geom.Vec2D,
+    Rect = toxi.geom.Rect;
+
+let NUM_PARTICLES = 100;
+
+let physics;
+let mouseAttractor;
+let mousePos;
+
+let headAttractor;
+let headPos;
+
 function setup() {
   createCanvas(w, h);
   video = createCapture(VIDEO);
@@ -20,15 +38,44 @@ function setup() {
   video.hide();
   fill(255);
   stroke(255);
+
+  physics = new VerletPhysics2D();
+  physics.setDrag(0.05);
+  physics.setWorldBounds(new Rect((0, 0, width, height)));
+  physics.addBehavior(new GravityBehavior(new Vec2D(0, 0.15)));
+
+  headPos = new Vec2D(width/2,height/2); 
+  headAttractor = new AttractionBehavior(headPos, 200, -0.9);
+  physics.addBehavior(headAttractor);
+}
+
+function addParticle() {
+  let randLoc = Vec2D.randomVector().scale(5).addSelf(width / 2, 0);
+  let p = new VerletParticle2D(randLoc);
+    physics.addParticle(p); 
+    physics.addBehavior(new AttractionBehavior(p, 20, -1.2, 0.01));
 }
 
 function draw() {
+  physics.update();
   image(video, 0, 0, w, h);
   drawKeypoints();
   drawSkeleton();
+
+  if (physics.particles.length < NUM_PARTICLES) {
+    addParticle();
+  }
+    
+  for (let i=0;i<physics.particles.length;i++) {
+    let p = physics.particles[i];
+    fill(255);
+    noStroke();
+    ellipse(p.x, p.y, 10, 10);
+  }
+
+  noStroke();
+  text(int(frameRate()), 30, 30);
 }
-
-
 
 function drawSkeleton() {
   for(let i = 0; i < poses.length; i++) {
@@ -45,7 +92,15 @@ function drawKeypoints() {
     for(let j = 0; j < poses[i].pose.keypoints.length; j++) {
       let keypoint = poses[i].pose.keypoints[j];
       if (keypoint.score > 0.2) {
+        fill(255,0,100);
+        noStroke();
         ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+        if(j==0) {
+          headPos.set(keypoint.position.x, keypoint.position.y);
+          noFill();
+          stroke(255,255,0);
+          ellipse(keypoint.position.x, keypoint.position.y,200,200);
+        }
       }
     }
   }
